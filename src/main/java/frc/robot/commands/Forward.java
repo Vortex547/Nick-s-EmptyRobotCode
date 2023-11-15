b8256810013f6906c767cb831c4955b519f148d9
Vortex547 <gashni26@spaldinghs.org>
2023-11-15 18:35:40 -0500

    Finished Robot Coding

--- src/main/java/frc/robot/commands/Forward.java
+++ src/main/java/frc/robot/commands/Forward.java
@@ -1,5 +1,22 @@
 package frc.robot.commands;
 
-public class Forward {
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Robot;
+import frc.robot.subsystems.Wheels;
+
+public class Forward extends CommandBase {
     
+    public Forward()
+    {
+        this.addRequirements(Robot.wheels);
+    }
+
+    public void execute()
+    {
+        if(Robot.wheels.getWheelSwitch() == false)
+        {
+            Robot.wheels.setLeftWheelMotorState(Wheels.LeftWheelMotorState.ON);
+            Robot.wheels.setRightWheelMotorState(Wheels.RightWheelMotorState.ON);
+        }
+    }
 }
